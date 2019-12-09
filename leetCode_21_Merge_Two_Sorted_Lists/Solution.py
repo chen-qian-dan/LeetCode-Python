8123544c275e3e8a8986a10f25a63f3d9a04e789
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-10 08:21:07 +1030

    leetCode_21_Merge two sorted lists.

---
+++ leetCode_21_Merge_Two_Sorted_Lists/Solution.py
@@ -0,0 +1,49 @@
+
+'''
+Qian Chen
+20191210Tue
+'''
+
+from leetCode_21_Merge_Two_Sorted_Lists.ListNode import *
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode):
+        '''
+        :param l1: ListNode
+        :param l2: ListNode
+        :return: ListNode
+        '''
+        head = cur = ListNode(0)
+
+        if l1 == None and l2 == None:
+            return None
+        elif l1 == None and l2 != None:
+            return l2
+        elif l1 != None and l2 == None:
+            return l1
+
+        while l1 != None and l2 != None:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1 != None:
+            cur.next = l1
+        else:
+            cur.next = l2
+
+        return head.next
+
+
+
+
+
+
+
+
+
+
+
